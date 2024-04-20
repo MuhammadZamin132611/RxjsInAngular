74054b298f26bb256de464a8cd01252c24762654
Zamin <Muhammad.Zamin@pratian.com>
2024-04-20 20:10:13 +0530

    Add toArray() functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ListComponent } from './observable/list/list.component';
 import { FromEventComponent } from './observable/from-event/from-event.component';
 import { IntervalComponent } from './observable/interval/interval.component';
 import { OfFromComponent } from './observable/of-from/of-from.component';
+import { ToArrayComponent } from './observable/to-array/to-array.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
     {path: 'from-event', component:FromEventComponent},
     {path: 'interval', component:IntervalComponent},
     {path: 'of-from', component:OfFromComponent},
+    {path: 'to-array', component:ToArrayComponent},
   ]},
   {path: '**', redirectTo:'promise'}
 ];
--- src/app/observable/to-array/to-array.component.ts
+++ src/app/observable/to-array/to-array.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, from, interval, of, skip, take, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-to-array',
   templateUrl: './to-array.component.html',
   styleUrls: ['./to-array.component.scss']
 })
-export class ToArrayComponent {
+export class ToArrayComponent implements OnInit {
 
+  constructor(){}
+
+  sourceSub!: Subscription
+  ngOnInit(): void {
+
+    // Ex - 01
+    const source = interval(1000)
+    this.sourceSub = source.pipe(
+      take(5), toArray())
+    .subscribe(res=>{
+      // console.log(res)
+
+      // if(res>=8){
+      //   this.sourceSub.unsubscribe()
+      // }
+    })
+
+
+    // Ex - 02
+    const source2 = from(this.users);
+
+    source2.pipe(toArray()).subscribe(res=>{
+      // console.log(res)
+    })
+
+
+
+    // Ex - 03
+    const source3  = of('Anup', 'Shekhar', 'Sharma', 'Uxtrendsz');
+    source3.pipe(toArray()).subscribe(res=>{
+      console.log(res)
+    })
+
+  }
+
+  users = [
+    {name: 'Anup', skill: 'Angular'},
+    {name: 'Shekhar', skill: 'Html, CSS'},
+    {name: 'Sharma', skill:"JavaScript"},
+    {name: 'Uxtrentz', skill: 'TypeScript'}
+  ]
+  
 }
